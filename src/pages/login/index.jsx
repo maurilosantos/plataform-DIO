d564b2bcf7b240a609b19833f182b94886c5507f
maurilosantos <maurilosantoss1@gmail.com>
2022-12-12 19:26:54 -0300

    final desafio

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,67 +1,103 @@
-import { useNavigate  } from "react-router-dom";
-import { MdEmail, MdLock } from 'react-icons/md'
-import { Button } from '../../components/Button';
-import { Header } from '../../components/Header';
-import { Input } from '../../components/Input';
-import { api } from '../../services/api';
-
+import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
+import Header from "../../components/Header/";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
 
-import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
+import {
+  Column,
+  Container,
+  CriarText,
+  EsqueciText,
+  Row,
+  SubtitleLogin,
+  Title,
+  TitleLogin,
+  Wrapper,
+} from "./styles";
 
-const Login = () => {
+import { MdLock, MdMail } from "react-icons/md";
 
-    const navigate = useNavigate()
+import { api } from "../../services/api";
 
-    const { control, handleSubmit, formState: { errors  } } = useForm({
-        reValidateMode: 'onChange',
-        mode: 'onChange',
-    });
+export default function Login() {
+  const navigate = useNavigate();
 
-    const onSubmit = async (formData) => {
-        try{
-            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
-            
-            if(data.length && data[0].id){
-                navigate('/feed') 
-                return
-            }
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    reValidateMode: "onChange",
+    mode: "onChange",
+  });
 
-            alert('Usuário ou senha inválido')
-        }catch(e){
-            //TODO: HOUVE UM ERRO
-        }
-    };
+  async function onSubmit(formData) {
+    try {
+      const { data } = await api.get(
+        `users?email=${formData.email}&senha=${formData.senha}`
+      );
 
-    console.log('errors', errors);
+      if (data.length && data[0].id) {
+        navigate("/feed");
+        return;
+      }
+      alert("Usuário ou senha inválidos.");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    return (<>
-        <Header />
-        <Container>
-            <Column>
-                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
-                 e entrar mais rápido nas empresas mais desejadas.</Title>
-            </Column>
-            <Column>
-                <Wrapper>
-                <TitleLogin>Faça seu cadastro</TitleLogin>
-                <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
-                    {errors.email && <span>E-mail é obrigatório</span>}
-                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
-                    {errors.senha && <span>Senha é obrigatório</span>}
-                    <Button title="Entrar" variant="secondary" type="submit"/>
-                </form>
-                <Row>
-                    <EsqueciText>Esqueci minha senha</EsqueciText>
-                    <CriarText>Criar Conta</CriarText>
-                </Row>
-                </Wrapper>
-            </Column>
-        </Container>
-    </>)
+  console.log(errors);
+
+  return (
+    <>
+      <Header />
+
+      <Container>
+        <Column>
+          <Title>
+            A plataforma para você aprender com experts, dominar as principais
+            tecnologias e entrar mais rápido nas empresas mais desejadas.
+          </Title>
+        </Column>
+
+        <Column>
+          <Wrapper>
+            <TitleLogin>Faça seu cadastro</TitleLogin>
+            <SubtitleLogin>Faça seu login e make the change.</SubtitleLogin>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Input
+                placeholder="E-mail"
+                leftIcon={<MdMail />}
+                name="email"
+                control={control}
+              />
+
+              {errors.email && <span>E-mail obrigatório</span>}
+
+              <Input
+                type="password"
+                placeholder="Senha"
+                leftIcon={<MdLock />}
+                name="senha"
+                control={control}
+              />
+
+              {errors.senha && <span>Senha obrigatória</span>}
+
+              <Button title="Entrar" variant="secondary" type="submit" />
+            </form>
+
+            <Row>
+              <EsqueciText>Esqueci minha senha</EsqueciText>
+              <CriarText>Criar Conta</CriarText>
+            </Row>
+          </Wrapper>
+        </Column>
+      </Container>
+    </>
+  );
 }
-
-export { Login }
